98a805a3ff663ff78f1d9e4afb6eeb087f2c63b4
christeh <hansen.christer@gmail.com>
2015-03-23 14:00:07 +0100

    - Lagt til execute på asynctask.

--- app/src/main/java/no/hinesna/asynctaskjson/LocationAsyncTask.java
+++ app/src/main/java/no/hinesna/asynctaskjson/LocationAsyncTask.java
@@ -1,7 +1,27 @@
 package no.hinesna.asynctaskjson;
 
+import android.os.AsyncTask;
+
+import java.net.URL;
+
 /**
  * Created by christerhansen on 23.03.15.
  */
-public class LocationAsyncTask {
+public class LocationAsyncTask extends AsyncTask<String, Integer, Long>{
+
+    @Override
+     protected void onPreExecute() {
+        super.onPreExecute();
+    }
+
+    @Override
+    protected Long doInBackground(String... params) {
+        return null;
+    }
+
+
+    @Override
+    protected void onPostExecute(Long aLong) {
+        super.onPostExecute(aLong);
+    }
 }
